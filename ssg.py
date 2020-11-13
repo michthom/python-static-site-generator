19c6147db5a51e4e282576fd6a4dc281394808ab
Michael Thomson <github.com@m-thomson.net>
2020-11-13 11:23:30 +0000

    More fixes

--- ssg.py
+++ ssg.py
@@ -1,11 +1,13 @@
 import typer
 from ssg.site import Site
 
+
 def main(source="content", dest="dist"):
-    config = (
+    config = {
         "source": source,
         "dest": dest
-    )
+    }
     site = Site(**config).build()
+
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -12,10 +12,10 @@ class Site:
         print(f'dest: {self.dest}')
         print(f'directory: {directory}')
 
-        Path.mkdir(directory, parents=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        self.mkdir(parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                path.create_dir()
+                self.create_dir(path)
