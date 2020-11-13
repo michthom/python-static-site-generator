7b1bb5d5d82dd71f9ae3334b204d35100c0f0d78
Michael Thomson <github.com@m-thomson.net>
2020-11-13 12:15:48 +0000

    Module 3 fix the first

--- ssg/content.py
+++ ssg/content.py
@@ -4,13 +4,30 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
         (_, fm, content) = Content.__regex.split(string, maxsplit=2)
 
-        Content.load(fm, Loader=FullLoader)
+        # yaml.load, this isn't recursive!
+        load(fm, Loader=FullLoader)
 
-        return(cls(metadata, content))
+        return cls(self.data, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data.append("content": content)
+
+    @property
+    def body():
+        return self.data["content"]
+
+    @property
+    def type():
+        return self.data["type"]
+
+    @type.setter
+    def set_data(data):
+        self.data["type"] = data
