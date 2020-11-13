5ee0e58da453883cb55e722eac4fb9715c22d9e7
Michael Thomson <github.com@m-thomson.net>
2020-11-13 13:53:05 +0000

    Module 4 done

--- ssg/site.py
+++ ssg/site.py
@@ -37,5 +37,5 @@ class Site:
             self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
     @staticmethod
-    def error(message="Default message"):
-        sys.stdout.write("\x1b[1;31m{}".format(message))
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
